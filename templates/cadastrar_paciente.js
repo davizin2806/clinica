e1bbb537074032f2fbe10c256c501b32f430724d
Mateus Fonseca <159179957+fonsca@users.noreply.github.com>
2025-11-18 16:04:22 -0300

    faltou esse

--- templates/cadastrar_paciente.js
+++ templates/cadastrar_paciente.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://172.20.14.138:5000'; // ⚠️ MUDE AQUI SE O IP MUDAR
+const API_URL = 'http://192.168.1.14:5000'; // ⚠️ MUDE AQUI SE O IP MUDAR
 
         // --- Carregar Convênios no Dropdown ---
         document.addEventListener('DOMContentLoaded', function() {
